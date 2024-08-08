53e14546b5a808f6504b3231b979fde89361e00f
enforcer <120410051+diverenforce@users.noreply.github.com>
2024-08-08 21:55:06 +0630

    implement test for small

--- test_bank.py
+++ test_bank.py
@@ -6,3 +6,9 @@ def test_value_caps():
     assert value('WHATDOYOUMEAN????') == 100
     assert value('HOWIS THIS POSSIBLE?!!') == 20
     assert value('HELLO TO THE UNKNOWN') == 0
+
+def test_value_small():
+    assert value('joking hahahaha') == 100
+    assert value('whatdoyoumean????') == 100
+    assert value('howis this possible?!!') == 20
+    assert value('hello to the unknown') == 0
